9e7491457dcca0a26727841144785e32782f3a87
Jason Adam Young <jayoung@extension.org>
2013-07-30 17:43:37 -0400

    run things!

--- lib/cronmon/cron.rb
+++ lib/cronmon/cron.rb
@@ -2,6 +2,7 @@
 # Copyright (c) 2013 North Carolina State University
 # === LICENSE:
 # see LICENSE file
+require 'open3'
 
 module Cronmon
   class Cron
@@ -19,39 +20,18 @@ module Cronmon
     end
 
     def run
-      # fake
-      @stdout = 'output testing'
-      @stderr = 'error testing'
-      @start = Time.now.utc - 3600
-      @finish = Time.now.utc
-      @success = true
+      @results['start'] = Time.now.utc
+      stdin, stdout, stderr = Open3.popen3(@command)
+      stdin.close
+      @results['stdout'] = stdout.read
+      @results['stderr'] = stderr.read
+      @results['finish'] = Time.now.utc
+      @results['success'] = stderr.empty?
     end
 
+
     def log
-      client = OAuth2::Client.new(@options.auth.uid, @options.auth.secret, {site: @options.posturi, raise_errors: false})
-      if(token = client.client_credentials.get_token)
-        postdata = {label: @label, 
-                    command: @command, 
-                    start: @start,
-                    finish: @finish,
-                    success: true,
-                    stdout: @stdout, 
-                    stderr: @stderr}
-        response = token.post('cronmons/log', body: postdata)
-        # if(response)
-        #   if(response.status == 200)
-        #     return registration_success(response)
-        #   elsif(response.status == 422)
-        #     response_data = JSON.parse(response.body)
-        #     if(response_data['message'])
-        #       response_data['message']
-        #     end
-        #   else
-        #     response
-        #   end
-        # end
-        response
-      end
+
     end
   end
 
